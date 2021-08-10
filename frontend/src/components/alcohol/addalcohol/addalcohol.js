39db5643a4da8e0542a4bcea73d1db810e644779
mooatib <axel.raux@gmail.com>
2021-08-10 14:01:21 +0200

    Updating Add Alcohol Form

--- frontend/src/components/alcohol/addalcohol/addalcohol.js
+++ frontend/src/components/alcohol/addalcohol/addalcohol.js
@@ -1,25 +1,54 @@
 import './addalcohol.css'
 import { Add } from '@material-ui/icons'
+import { useState } from 'react'
 
 export default function AddAlcohol() {
+    const [nameInput, setNameInput] = useState('')
+    const [typeInput, setTypeInput] = useState('')
+    const [percentageInput, setPercentageInput] = useState('')
 
+    const handleClick = () => {
+        if (!nameInput || !typeInput || !percentageInput)
+            alert("Veuillez remplir entierement le formulaire !")
+    }
+
+    const handleChange = (label) => (e) => {
+        switch (label) {
+            case "nom":
+                setNameInput(e.target.value);
+                break;
+            case "type":
+                setTypeInput(e.target.value);
+                break;
+            case "pourcentage":
+                setPercentageInput(e.target.value);
+                break;
+            default :
+        }
+    }
     return (
         <div className="add-alcohol-container">
-            <form className="add-alcohol-form">
-                <button name="Create" type="button"><Add /></button>
+            <div className="add-alcohol-form">
+                <button name="Voir" type="button"><Add /></button>
                 <div>
                     <label for="name">Nom :</label>
-                    <input type="text" id="type" name="type" required />
+                    <input type="text" onChange={handleChange("nom")} />
                 </div>
-                <div>                
+                <div>
                     <label for="type">Type :</label>
-                    <input type="text" id="name" name="name" required />
+                    <select onChange={handleChange("type")}>
+                        <option value="0">Bière</option>
+                        <option value="1">Alcool fort</option>
+                    </select>
                 </div>
                 <div>
                     <label for="percent">Pourcentage :</label>
-                    <input type="number" id="percent" name="percent" required />
+                    <input type="number" onChange={handleChange("pourcentage")} />
                 </div>
-            </form>
+                <div>
+                    <button type="button" name="Ajouter" onClick={handleClick}><Add /></button>
+                </div>
+            </div>
         </div>
     )
 }
