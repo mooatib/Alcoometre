bc3b01b5f99dd92b604bb663ddc8d8729de68418
mooatib <axel.raux@gmail.com>
2021-08-10 09:51:50 +0200

    Updating alcohol list

--- frontend/src/components/alcohol/alcohollist/alcohollist.js
+++ frontend/src/components/alcohol/alcohollist/alcohollist.js
@@ -1,19 +1,26 @@
 import './alcohollist.css'
-import { useSelector} from 'react-redux'
-
+import { useSelector } from 'react-redux'
+import {TiBeer} from 'react-icons/ti'
+import {FaCocktail} from 'react-icons/fa'
 
 export default function AlcoholsList() {
     const alcoholsList = useSelector((state) => state.alcoholsReducer)
 
+    function alcoholType(type) {
+        if(type === '0')
+            return <TiBeer/>
+        else
+            return <FaCocktail/>
+    }
     return (
         <div className="alcohol-list-container">
             <ul className="alcohol-list">
                 {alcoholsList.map((alcohol) => {
                     return (
-                        <li className="alcohol" key={alcohol.aid}>
+                        <li key={alcohol.aid}>
+                            <p>{alcoholType(alcohol.type)}</p>
                             <p>{alcohol.name}</p>
-                            <p>{alcohol.type}</p>
-                            <p>{alcohol.percentage}</p>
+                            <p>{alcohol.percentage}%</p>
                         </li>
                     )
                 })}
