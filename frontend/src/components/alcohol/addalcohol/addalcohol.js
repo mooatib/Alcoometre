1c0ad6ccc6e2b9368ae2eae8b2a7dcb256d4560a
mooatib <axel.raux@gmail.com>
2021-08-10 12:09:17 +0200

    Updating New Alcohol Form

--- frontend/src/components/alcohol/addalcohol/addalcohol.js
+++ frontend/src/components/alcohol/addalcohol/addalcohol.js
@@ -1,20 +1,25 @@
 import './addalcohol.css'
 import { Add } from '@material-ui/icons'
-/* import {Person, List, Add } from '@material-ui/icons' */
-
 
 export default function AddAlcohol() {
 
     return (
         <div className="add-alcohol-container">
-            <ul className="add-alcohol-form">
-                <li className="alcohol-form-input">
-                    <button name="Create" type="button"><Add /></button>
-                </li>
-                <li className="alcohol-form-input">
-                    <input type="text"/>
-                </li>
-            </ul>
+            <form className="add-alcohol-form">
+                <button name="Create" type="button"><Add /></button>
+                <div>
+                    <label for="name">Nom :</label>
+                    <input type="text" id="type" name="type" required />
+                </div>
+                <div>                
+                    <label for="type">Type :</label>
+                    <input type="text" id="name" name="name" required />
+                </div>
+                <div>
+                    <label for="percent">Pourcentage :</label>
+                    <input type="number" id="percent" name="percent" required />
+                </div>
+            </form>
         </div>
     )
 }
