93a5435d8c0db4d94b6591ca27cc87574a28f612
mooatib <axel.raux@gmail.com>
2021-08-12 14:52:50 +0200

    Removing redirect link on topbar

--- frontend/src/components/topbar/topbar.jsx
+++ frontend/src/components/topbar/topbar.jsx
@@ -1,18 +1,12 @@
 import './topbar.css'
-import { useHistory } from 'react-router-dom'
 import { FaUmbrellaBeach } from 'react-icons/fa'
 
 export default function Topbar() {
-    const history = useHistory()
-
-    const handleClick = () => e => {
-        history.push("/")
-    }
 
     return (
         <div className="topbar-container">
             <div className="topbar-left">
-                <span onClick={handleClick()}><FaUmbrellaBeach/> Soûlac</span>
+                <span><FaUmbrellaBeach/> Soûlac</span>
             </div>
             <div className="topbar-right">
                 <span>Qui boit quoi ?</span>
