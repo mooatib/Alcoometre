5dfd9df0f058c8351cfbb2f3904342101590b28a
mooatib <axel.raux@gmail.com>
2021-08-17 11:13:38 +0200

    Fixing issue with charts

--- routes/stats.js
+++ routes/stats.js
@@ -1,6 +1,12 @@
 const router = require('express').Router()
 const db = require('../db/db.js')
 
+const formatDate = (d) => {
+    const date = new Date(d)
+    const datestring = date.toISOString().split('T')[0] + ' ' + date.getHours() + ":" + date.getMinutes()+ ":" + date.getSeconds()
+    return datestring
+}
+
 const f = (rate, username,) => {
     const sql = `UPDATE users SET alcohol = ? WHERE username = ?`
     const params = [username, rate]
@@ -26,13 +32,13 @@ const groupbars = (data) => {
     return dataset
 }
 
-const grouprate = (data, step) => {
+const grouprate = (data, step, dateinit) => {
     const dataset = []
     for (var x = 1; x <= 10; x++) {
         const userdataset = data.filter(function (e) {
             return e.uid == x
         })
-        dataset.push(buildDataSet(calcRate(userdataset, step), step))
+        dataset.push(buildDataSet(calcRate(userdataset, step), step, dateinit))
     }
     return dataset
 }
@@ -46,18 +52,18 @@ const calcRate = (data, step) => {
             sex = 0.7
         date = new Date(element.date)
         deg = (element.quantity * 10 * element.percentage / 100 * 0.8) / (sex * element.weight)
+        console.log(deg)
         dataset.push({ username: element.username, timestamp: date, rm: deg / step, ttl: 3600000 })
     })
     return dataset
 }
 
-const buildDataSet = (data, step) => {
+const buildDataSet = (data, step, dateinit) => {
     const dataset = []
-    var now = new Date()
     var alcohol_grams = 0
     var rd = 0.1 / step
     var user = null
-    for (let t = new Date().setDate(now.getDate() - 1); t <= Date.parse(now); t += 3600000 / step) {
+    for (let t = dateinit; t <= Date.parse(new Date()); t += 3600000 / step) {
         data.forEach(element => {
             user = element.username
             if (t >= element.timestamp && element.ttl > 0) {
@@ -79,40 +85,45 @@ const buildDataSet = (data, step) => {
 router.get('/userrate', (req, res, next) => {
     var data = {
         uid: req.query.uid,
-        step: req.query.step
+        step: req.query.step,
+        date: new Date().setDate(new Date().getDate() - 2)
     }
+    const date = formatDate(data.date)
     const sql = `SELECT did, username, sex, weight, percentage, quantity, date from users
                 JOIN drinks ON drinks.uid = users.uid
                 JOIN alcohol ON drinks.aid = alcohol.aid
-                WHERE drinks.uid = ?`
-    const params = [data.uid]
+                WHERE drinks.uid = ? AND date > ?`
+    const params = [data.uid, date]
     db.all(sql, params, (err, result) => {
         if (err) {
             res.status(400).json({ "error": err.message })
             return
         }
         const array = JSON.parse(JSON.stringify(result))
-        const dataset = buildDataSet(calcRate(array, data.step), data.step)
+        const dataset = buildDataSet(calcRate(array, data.step,), data.step, data.date)
         res.status(200).send(dataset)
     })
 })
 
 router.get('/grouprate', (req, res, next) => {
     var data = {
-        step: req.query.step
+        step: req.query.step,
+        date: new Date().setDate(new Date().getDate() - 2)
     }
+    const date = formatDate(data.date)
     const sql = `SELECT users.uid, username, sex, weight, did, percentage, quantity, date from users
                 JOIN drinks ON drinks.uid = users.uid
                 JOIN alcohol ON drinks.aid = alcohol.aid
+                WHERE date > ?
                 ORDER BY drinks.uid ASC`
-    const params = []
+    const params = [date]
     db.all(sql, params, (err, result) => {
         if (err) {
             res.status(400).json({ "error": err.message })
             return
         }
         const array = JSON.parse(JSON.stringify(result))
-        const dataset = grouprate(array, data.step)
+        const dataset = grouprate(array, data.step, data.date)
         res.status(200).send(dataset)
     })
 })
